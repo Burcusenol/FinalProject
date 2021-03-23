3db59aeebc7504bcbd6627b82fe1bbfb10829334
Burcu ŞENOL <burcusenol62@gmail.com>
2021-03-23 16:40:56 +0300

    23.03.2021

--- Entities/Abstract/IEntity.cs
+++ Entities/Abstract/IEntity.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Entities.Abstract
 {
-    //IEntity implement eden class bir veitabanı tablosudur.
+    //IEntity implement eden class bir veritabanı tablosudur.
     public interface IEntity
     {
     }
