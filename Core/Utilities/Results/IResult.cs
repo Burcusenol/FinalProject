c293772a7533d632b551570f4e21b02b96f29b43
Burcu ŞENOL <burcusenol62@gmail.com>
2021-03-29 19:30:21 +0300

    29.03.2021

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    //Temel voidler için başlangıç
+    public interface IResult
+    {
+        bool Success { get; }//Sadece okunabilir
+        string Message { get; } //Kullanıcı mesajı
+
+
+    }
+}
