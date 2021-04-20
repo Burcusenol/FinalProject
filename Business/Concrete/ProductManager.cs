b9423661f47acb76d2bf2c9137c0c28f212eedd4
Burcu ŞENOL <burcusenol62@gmail.com>
2021-04-20 17:46:20 +0300

    20.04.2021

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -42,7 +42,7 @@ namespace Business.Concrete
        //[Validate]
 
 
-        [SecuredOperation("product.add,admin")]
+        //[SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
---
+++ Core/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+
+namespace Core.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureCustomExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
